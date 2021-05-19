2c318ab5bfb3b5275a9d88a605139d30cb0a5c4d
baptiste-gauthier <baptiste.gauthier@laplateforme.io>
2021-05-19 12:05:50 +0200

    Create profil.php

---
+++ pages/profil.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title> Page profil </title>
+    </head>
+
+    <body>
+        <section id="profil">
+            <article class="contenu_profil">
+                <div class="img_description">
+                    <div>
+                        <img src="../img/pp.jpg" alt="#">
+                    </div>
+                    <h1> Baptiste Gauthier </h1>
+                    <p> @baptistegauthier </p>
+                </div>
+
+                <div class="btn_modif_profil">
+                    <button id="modif"> Modifier mon profil </button>
+                </div>
+
+                
+            </article>
+        </section>
+    </body>
+
+</html>
