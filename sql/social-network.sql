0e1f8c0258ea639bc92f45c497c3fc83b51e1558
alexandre-leroux <71876330+alexandre-leroux@users.noreply.github.com>
2021-05-21 00:12:53 +0200

    Update social-network.sql

--- sql/social-network.sql
+++ sql/social-network.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 09, 2021 at 11:45 PM
+-- Generation Time: May 20, 2021 at 09:56 PM
 -- Server version: 5.7.24
 -- PHP Version: 7.4.1
 
@@ -25,6 +25,30 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `chat_prive`
+--
+
+CREATE TABLE `chat_prive` (
+  `id` int(11) NOT NULL,
+  `fk_id_user_1` int(11) NOT NULL,
+  `fk_id_user_2` int(11) NOT NULL,
+  `fk_id_auteur_message` int(11) NOT NULL,
+  `message` text NOT NULL,
+  `date` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `chat_prive`
+--
+
+INSERT INTO `chat_prive` (`id`, `fk_id_user_1`, `fk_id_user_2`, `fk_id_auteur_message`, `message`, `date`) VALUES
+(1, 2, 3, 1, 'salut ça va ?', '2021-05-20 18:22:59'),
+(2, 2, 3, 2, 'oui et toi ?', '2021-05-20 18:23:15'),
+(3, 3, 2, 2, 'sinon quoi de neuf ?', '2021-05-20 19:11:48');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
@@ -40,7 +64,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `prenom`, `mail`, `connecte`) VALUES
-(2, 'alex', 'a@a.a', 0),
+(2, 'alex', 'a@a.a', 1),
 (3, 'boris', 'b@b.b', 0),
 (4, 'jean', 'c@c.c', 0),
 (5, 'roger', 'd@d.d', 0),
@@ -52,6 +76,12 @@ INSERT INTO `users` (`id`, `prenom`, `mail`, `connecte`) VALUES
 --
 
 --
+-- Indexes for table `chat_prive`
+--
+ALTER TABLE `chat_prive`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
@@ -60,6 +90,12 @@ ALTER TABLE `users`
 --
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `chat_prive`
+--
+ALTER TABLE `chat_prive`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
