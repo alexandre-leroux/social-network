f42063917f6d93200071f2fd2237983097c184f4
alexandre-leroux <71876330+alexandre-leroux@users.noreply.github.com>
2021-05-21 12:10:56 +0200

    base message non lu

--- scripts/chat_prive.js
+++ scripts/chat_prive.js
@@ -1,6 +1,8 @@
 displayUser()
 checkUsersConnect()
 setInterval(checkUsersConnect, 10000)
+setTimeout(eventsurclick, 20)
+checkNewMessage()
 
 
 function displayUser(){
@@ -74,7 +76,7 @@ function checkUsersConnect(){
             let i = 0;
             while ( i < count)
             {
-                console.log(utilisateurs[i].lastChild)
+                // console.log(utilisateurs[i].lastChild)
                 // utilisateurs[i].lastChild.classList.add("connecte");
                 if(dataType[i]["connecte"] == 1)
                 {
@@ -105,55 +107,21 @@ function checkUsersConnect(){
 
 
 
-function eventsurclick(){
+function checkNewMessage(){
 
 
-        utilisateurs = document.getElementsByClassName("users");
 
-       
-        for(i = 0; i<utilisateurs.length; i++)
-        {
-            // console.log(utilisateurs[i].lastChild)
-            let node = utilisateurs[i].lastChild.innerHTML
-            // console.log(node)
-
-            let pseudo =  utilisateurs[i].lastChild.innerHTML
-
-            utilisateurs[i].addEventListener('click', function(){
-
-                console.log(pseudo)
-                parent2 = document.getElementById('conteneur_des_messages')
-                parent2.innerHTML = "" 
 
                 $.ajax({
-                    url: "scripts_ajax_php/chat_prive.php",
+                    url: "scripts_ajax_php/chat_prive_new_message.php",
                     type: "POST",
-                    data: {  "pseudo":pseudo,  },
-                    dataType: "JSON",
+                    // data: {  "pseudo":pseudo,  },
+                    dataType: "text",
                   
                     success : function(dataType){
             
-                        console.log(dataType.data1)
-                        console.log(dataType.data2)
-                        parent = document.getElementById('user_selection_chat')
-                        // console.log(parent)
-                        var p = document.createElement("p");
-                        p.innerHTML =  dataType.data1[1]
-                        parent.innerHTML = ""
-                        parent.appendChild(p);
+                        console.log(dataType)
 
-                        var mess = document.createElement("p");
-                        console.log(dataType.data2.length)
-
-                        for (i=0; i<dataType.data2.length; i++)
-                        {
-                            console.log('dans foir')
-                            parent2 = document.getElementById('conteneur_des_messages')
-                            var mess = document.createElement("p");
-                             contenur_mess = dataType.data2[i][4]
-                             mess.innerHTML = contenur_mess
-                             parent2.appendChild(mess);
-                        }
                     
                     
                     },
@@ -168,17 +136,84 @@ function eventsurclick(){
                     }
             
             
-                })
+        })
+
+}
 
 
+
+
+
+
+function eventsurclick(){
+
+
+    utilisateurs = document.getElementsByClassName("users");
+
+   
+    for(i = 0; i<utilisateurs.length; i++)
+    {
+        // console.log(utilisateurs[i].lastChild)
+        let node = utilisateurs[i].lastChild.innerHTML
+        // console.log(node)
+
+        let pseudo =  utilisateurs[i].lastChild.innerHTML
+
+        utilisateurs[i].addEventListener('click', function(){
+
+            console.log(pseudo)
+            parent2 = document.getElementById('conteneur_des_messages')
+            parent2.innerHTML = "" 
+
+            $.ajax({
+                url: "scripts_ajax_php/chat_prive.php",
+                type: "POST",
+                data: {  "pseudo":pseudo,  },
+                dataType: "JSON",
+              
+                success : function(dataType){
+        
+                    console.log(dataType.data1)
+                    console.log(dataType.data2)
+                    parent = document.getElementById('user_selection_chat')
+                    // console.log(parent)
+                    var p = document.createElement("p");
+                    p.innerHTML =  dataType.data1[1]
+                    parent.innerHTML = ""
+                    parent.appendChild(p);
+
+                    var mess = document.createElement("p");
+                    console.log(dataType.data2.length)
+
+                    for (i=0; i<dataType.data2.length; i++)
+                    {
+                        console.log('dans foir')
+                        parent2 = document.getElementById('conteneur_des_messages')
+                        var mess = document.createElement("p");
+                         contenur_mess = dataType.data2[i][4]
+                         mess.innerHTML = contenur_mess
+                         parent2.appendChild(mess);
+                    }
+                
+                
+                },
+            
+                error: function (request, status, error) {
+                    // console.log(request.responseText);
+                },
+            
+                complete : function(resultat, statut){
+                    // console.log(resultat);
+                    // console.log(statut);
+                }
+        
+        
             })
-        }
+
+
+        })
+    }
 
 
 
 }
-
-setTimeout(eventsurclick, 20)
-
-
-
---
+++ scripts_ajax_php/chat_prive_new_message.php
@@ -0,0 +1,36 @@
+<?php
+session_start();
+require_once('../model/database.php');
+
+// $requete = $bdd->prepare('SELECT * FROM users WHERE prenom = ?');
+// $requete->execute([$_POST['pseudo']]);
+// $user = $requete->fetch();
+// var_dump($user);
+// echo json_encode($user);
+
+
+//  $id_user_selectionne = $user['id'];
+ $id_moi = $_SESSION['id'];
+// var_dump($id_user_selectionne);
+
+$recherche_discussion = $bdd->prepare('SELECT  fk_id_auteur_message FROM chat_prive 
+                                        WHERE  (fk_id_user_1 = ?  OR  fk_id_user_2 = ?) AND fk_id_auteur_message != ? AND non_lu = 1  ');
+                                     
+
+$recherche_discussion->execute(array($id_moi, $id_moi, $id_moi ));   
+
+$resultat_message_non_lu = $recherche_discussion->fetchAll();
+
+
+
+
+
+
+var_dump($resultat_message_non_lu); 
+// echo json_encode($resultat_message);                         
+
+
+
+// echo json_encode(array('data1' => $user, 'data2' => $resultat_message));
+
+
