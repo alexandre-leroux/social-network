2b0b71fd5f1a85bf08b064e6b6f7d0d9498040be
baptiste-gauthier <baptiste.gauthier@laplateforme.io>
2021-05-31 16:44:00 +0200

    ajout vue comment

---
+++ libraries/Vue/Comment.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Vue ; 
+
+class Comment {
+
+    public function displayLastComment($array){
+
+
+        foreach($array as $key => $value)
+        ?>
+
+        <div class="contenu_comment">
+            <div class="comment">
+                <div id="img_comment">
+                    <img src="img/<?= $value['avatar'] ; ?>" alt="#">
+                </div>
+                <div id="description_comment">
+                    <p class="bold"> <?= $value['prenom'] ; ?> </p>
+                    <p> <?= $value['comment'] ; ?></p>
+                </div>
+            </div>   
+        </div> 
+
+        <?php   
+    }
+}
