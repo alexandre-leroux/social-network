10926fe5a0acd569d9dd91bd5524b69d86e8648c
alexandre-leroux <71876330+alexandre-leroux@users.noreply.github.com>
2021-06-01 23:30:39 +0200

    ok

--- scripts/fonction_chat_depuis_index.js
+++ scripts/fonction_chat_depuis_index.js
@@ -27,7 +27,7 @@ function displayUser(){
 
                 if(dataType[i]["connecte"] == 0)
                 {
-                    $('#users_list').append("<div id=\""+dataType[i][0]+"\" class=\"users\"><img src=\"img/"+dataType[i][5]+"\"><a href='pages/profil.php'><p class='p_liste_user'>"+dataType[i][1]+"</p></a><a href='pages/chat.php?pseudo="+dataType[i][1]+"'><img class='img_mail_user' src='img/email.png'></a></div>")
+                    $('#users_list').append("<div id=\""+dataType[i][0]+"\" class=\"users\"><img class='img_avatar_chat' src=\"img/"+dataType[i][5]+"\"><a href='pages/profil.php'><p class='p_liste_user'>"+dataType[i][1]+"</p></a><a href='pages/chat.php?pseudo="+dataType[i][1]+"'><img class='img_mail_user' src='img/email.png'></a></div>")
                 }
                 else if(dataType[i]["connecte"] == 1)
                 {
