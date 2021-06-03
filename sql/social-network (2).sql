973c7e5f2dfdfa3c038596c662c8df3d4e40e17e
alexandre-leroux <71876330+alexandre-leroux@users.noreply.github.com>
2021-06-03 22:46:13 +0200

    sql

--- sql/social-network (2).sql
+++ sql/social-network (2).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Jun 02, 2021 at 07:44 AM
+-- Generation Time: Jun 03, 2021 at 08:45 PM
 -- Server version: 5.7.24
 -- PHP Version: 7.4.1
 
@@ -57,40 +57,16 @@ CREATE TABLE `chat_prive` (
 --
 
 INSERT INTO `chat_prive` (`id`, `fk_id_user_1`, `fk_id_user_2`, `fk_id_auteur_message`, `message`, `date`, `non_lu`) VALUES
-(1, 2, 3, 3, 'salut ça va ?', '2021-05-20 18:22:59', 0),
-(2, 2, 3, 2, 'oui et toi ?', '2021-05-20 18:23:15', 0),
-(3, 3, 2, 2, 'sinon quoi de neuf ?', '2021-05-20 19:11:48', 0),
-(6, 2, 4, 4, 'Salut alex', '2021-05-21 11:53:03', 0),
-(7, 5, 2, 5, 'hello la forme ?', '2021-05-21 11:54:41', 0),
-(8, 5, 2, 5, 'et je t\'ai pas dit ', '2021-05-21 11:56:41', 0),
-(9, 3, 2, 3, 'coucou', '2021-05-25 07:10:41', 0),
-(10, 2, 3, 2, 'ça va?', '2021-05-25 07:10:55', 0),
-(11, 4, 3, 4, 'hello', '2021-05-25 07:11:43', 0),
-(12, 4, 3, 4, 't\'es là ?', '2021-05-25 07:12:26', 0),
-(13, 4, 4, 4, 'coucou', '2021-05-25 07:23:28', 0),
-(14, 4, 2, 4, 'hey', '2021-05-25 07:23:56', 0),
-(15, 2, 4, 2, 'ca va?', '2021-05-25 07:24:04', 0),
-(16, 4, 2, 4, 'oui et toi', '2021-05-25 07:24:23', 0),
-(17, 2, 4, 2, 'ouio', '2021-05-25 07:24:36', 0),
-(18, 2, 4, 2, 'coucou', '2021-05-25 07:28:04', 0),
-(19, 4, 2, 4, 'ca av', '2021-05-25 07:28:09', 0),
-(20, 2, 4, 2, 'zedfzef', '2021-05-25 07:28:14', 0),
-(21, 4, 6, 4, 'sdsdv', '2021-05-25 07:28:33', 0),
-(22, 2, 4, 2, 'ddd', '2021-05-25 07:28:36', 0),
-(23, 4, 2, 4, 'eeee', '2021-05-25 07:28:49', 0),
-(24, 2, 4, 2, 'eeeee', '2021-05-25 07:28:53', 0),
-(25, 4, 2, 4, 'fvdfvd', '2021-05-25 07:30:27', 0),
-(26, 2, 4, 2, 'ttttttttt', '2021-05-25 07:30:31', 0),
-(27, 2, 4, 2, 'ergerg', '2021-05-25 07:30:41', 0),
-(28, 4, 2, 4, 'erf', '2021-05-25 07:30:49', 0),
-(29, 2, 4, 2, 'ererf', '2021-05-25 07:30:51', 0),
-(30, 2, 1, 2, 'hello', '2021-06-01 08:15:25', 0),
-(31, 2, 1, 2, 'hgfhfgh', '2021-06-01 08:19:53', 0),
-(32, 1, 2, 1, 'fghfgh', '2021-06-01 08:20:02', 0),
-(33, 1, 2, 1, 'fghfgh', '2021-06-01 08:20:06', 0),
-(34, 1, 6, 1, 'zefzef', '2021-06-01 08:23:11', 1),
-(35, 1, 6, 1, 'zefzef', '2021-06-01 08:23:25', 1),
-(36, 1, 2, 1, 'zefzef', '2021-06-01 08:23:31', 0);
+(34, 1, 6, 1, 'zefzef', '2021-06-01 08:23:11', 0),
+(35, 1, 6, 1, 'zefzef', '2021-06-01 08:23:25', 0),
+(37, 1, 15, 3, 'zefzef', '2021-06-01 08:23:31', 0),
+(38, 6, 1, 6, 'hey', '2021-06-02 19:49:28', 0),
+(39, 1, 6, 1, 'oui?', '2021-06-02 19:49:43', 0),
+(40, 6, 1, 6, 'arrete', '2021-06-02 19:50:04', 0),
+(41, 15, 1, 15, 'j;hj;', '2021-06-03 10:15:48', 0),
+(51, 1, 7, 1, 'gfdg', '2021-06-03 13:38:15', 1),
+(52, 1, 8, 1, 'dbfb', '2021-06-03 13:39:42', 1),
+(53, 1, 10, 1, 'coucou', '2021-06-03 17:29:23', 1);
 
 -- --------------------------------------------------------
 
@@ -123,7 +99,8 @@ CREATE TABLE `groupe` (
 
 INSERT INTO `groupe` (`id`, `nom_du_groupe`) VALUES
 (1, 'tennis'),
-(2, 'natation');
+(2, 'natation'),
+(3, 'parmesan');
 
 -- --------------------------------------------------------
 
@@ -137,6 +114,15 @@ CREATE TABLE `messages_chat_groupe` (
   `id_groupe` int(11) NOT NULL,
   `message` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `messages_chat_groupe`
+--
+
+INSERT INTO `messages_chat_groupe` (`id`, `id_auteur`, `id_groupe`, `message`) VALUES
+(1, 1, 1, 'prout'),
+(2, 1, 1, 'fesf'),
+(3, 1, 3, 'hello');
 
 -- --------------------------------------------------------
 
@@ -189,7 +175,7 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `prenom`, `mail`, `connecte`, `nom`, `avatar`, `hobbies`, `mdp`) VALUES
-(1, 'Alexandre', 'alex@laplateforme.io', 0, 'Leroux', '1.png', 'chat\npneu\ntélé', 'Azer-1234'),
+(1, 'Alexandre', 'alex@laplateforme.io', 1, 'Leroux', '1.png', 'chat\npneu\ntélé', 'Azer-1234'),
 (2, 'boris', 'boris@laplateforme.io', 0, 'becker', '2.jpg', 'tennis\nsculpture sur blé', 'Azer-1234'),
 (6, 'rafa', 'rafa@laplateforme.io', 0, 'nadal', '6.jpg', 'tennis\npetanque', 'Azer-1234'),
 (7, 'John', 'John@laplateforme.io', 0, 'Snow', '7.jpg', 'hiver*\nmarcheur blanc', 'Azer-1234'),
@@ -202,7 +188,9 @@ INSERT INTO `users` (`id`, `prenom`, `mail`, `connecte`, `nom`, `avatar`, `hobbi
 (14, 'patrick', 'patrick@laplateforme.io', 0, 'sebastien', '14.jpg', 'humour\nchanson', 'Azer-1234'),
 (15, 'didier', 'didier@laplateforme.io', 0, 'super', '15.jpg', 'chanson\ntexte', 'Azer-1234'),
 (16, 'gargamel', 'gargamel@laplateforme.io', 0, 'garga', '16.jpg', 'schtroumph\nmagie', 'Azer-1234'),
-(17, 'gandalf', 'gandalf@laplateforme.io', 0, 'legris', '17.jpg', 'joaillerie\nvoyage', 'Azer-1234');
+(17, 'gandalf', 'gandalf@laplateforme.io', 0, 'legris', '17.jpg', 'joaillerie\nvoyage', 'Azer-1234'),
+(18, 'jacques', 'jacques@laplateforme.io', 0, 'chirac', '18.jpg', 'politique\nfraude', 'Azer-1234'),
+(19, 'dieu', 'dieu@laplateforme.io', 0, 'letoutpuissant', '19.jpg', 'dieu', 'Azer-1234');
 
 -- --------------------------------------------------------
 
@@ -222,12 +210,16 @@ CREATE TABLE `users_dans_groupe` (
 --
 
 INSERT INTO `users_dans_groupe` (`id`, `id_groupe`, `id_user`, `new_message`) VALUES
-(1, 1, 2, 0),
-(2, 1, 6, 0),
+(1, 1, 2, 1),
+(2, 1, 6, 1),
 (3, 1, 1, 0),
 (4, 2, 2, 0),
-(5, 2, 6, 1),
-(6, 2, 1, 0);
+(5, 2, 6, 0),
+(6, 2, 1, 0),
+(7, 3, 7, 1),
+(8, 3, 8, 1),
+(9, 3, 10, 1),
+(10, 3, 1, 0);
 
 --
 -- Indexes for dumped tables
@@ -301,7 +293,7 @@ ALTER TABLE `aime`
 -- AUTO_INCREMENT for table `chat_prive`
 --
 ALTER TABLE `chat_prive`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
 
 --
 -- AUTO_INCREMENT for table `comment`
@@ -313,13 +305,13 @@ ALTER TABLE `comment`
 -- AUTO_INCREMENT for table `groupe`
 --
 ALTER TABLE `groupe`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `messages_chat_groupe`
 --
 ALTER TABLE `messages_chat_groupe`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `post`
@@ -337,13 +329,13 @@ ALTER TABLE `post_image`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- AUTO_INCREMENT for table `users_dans_groupe`
 --
 ALTER TABLE `users_dans_groupe`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
